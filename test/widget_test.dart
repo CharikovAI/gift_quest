d4dd33598357800507a6635431cf6e01f1fa7eba
CharikovAI <sanchezzz-95@mail.ru>
2021-10-21 16:00:12 +0300

    add some tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,17 +4,29 @@ import 'package:provider/provider.dart';
 import 'package:gift_quest/main.dart';
 
 Widget createHomeScreen() => ChangeNotifierProvider<Data>(
-      create: (context) => Data(),
-      child: MaterialApp(
-        home: QuestApp(),
-      ),
-    );
+  create: (_) => Data(),
+  child: MaterialApp(
+    home: QuestApp()
+  ),
+);
 
 void main() {
-  group('Home Page Widget Tests', () {
+  group('Intro Page Widget Tests', () {
     testWidgets('Testing if greetings Text shows up', (tester) async {  
       await tester.pumpWidget(createHomeScreen());
+      expect(find.text('Greetings! This is app for creating and providing quests. If you have already got a QR code, please scan it and start your quest!'), findsOneWidget);
       expect(find.byType(ElevatedButton), findsOneWidget);
-    }); 
+      expect(find.byType(OutlinedButton), findsOneWidget);
+    });
+    testWidgets('Testing if elevated button shows up', (tester) async {  
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.text('I have QR code'), findsOneWidget);
+      expect(find.byType(ElevatedButton), findsOneWidget);
+    });
+    testWidgets('Testing if outlined button shows up', (tester) async {  
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.text('Create new quest'), findsOneWidget);
+      expect(find.byType(OutlinedButton), findsOneWidget);
+    });
   });
 }
