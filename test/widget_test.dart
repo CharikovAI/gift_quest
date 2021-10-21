78782ca0361fff8db828ff5c8aa7c651243901d3
CharikovAI <sanchezzz-95@mail.ru>
2021-10-21 12:39:27 +0300

    github actions added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import '../lib/main.dart';
-import '../lib/src/intro.dart';
+import 'package:gift_quest/main.dart';
 
 Widget createHomeScreen() => ChangeNotifierProvider<Data>(
       create: (context) => Data(),
