2b0ac33b0258571bf43d823c3d0127e50685bbe7
CharikovAI <sanchezzz-95@mail.ru>
2021-10-23 12:31:34 +0300

    upd intro screen tests

--- test/intro_test.dart
+++ test/intro_test.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gift_quest/src/intro.dart';
 import 'package:provider/provider.dart';
-import 'package:gift_quest/main.dart';
 import 'package:gift_quest/src/models/data.dart';
 
-Widget createHomeScreen() => ChangeNotifierProvider<Data>(
+Widget createIntroScreen() => ChangeNotifierProvider<Data>(
   create: (_) => Data(),
   child: const MaterialApp(
-    home: QuestApp()
+    home: Intro()
   ),
 );
 
 void main() {
   group('Intro Page Widget Tests', () {
     testWidgets('Testing if greetings Text shows up', (tester) async {  
-      await tester.pumpWidget(createHomeScreen());
+      await tester.pumpWidget(createIntroScreen());
       expect(find.text('Greetings! This is app for creating and providing quests. If you have already got a QR code, please scan it and start your quest!'), findsOneWidget);
       expect(find.byType(ElevatedButton), findsOneWidget);
       expect(find.byType(OutlinedButton), findsOneWidget);
     });
     testWidgets('Testing if elevated button shows up', (tester) async {  
-      await tester.pumpWidget(createHomeScreen());
+      await tester.pumpWidget(createIntroScreen());
       expect(find.text('I have QR code'), findsOneWidget);
       expect(find.byType(ElevatedButton), findsOneWidget);
     });
     testWidgets('Testing if outlined button shows up', (tester) async {  
-      await tester.pumpWidget(createHomeScreen());
+      await tester.pumpWidget(createIntroScreen());
       expect(find.text('Create new quest'), findsOneWidget);
       expect(find.byType(OutlinedButton), findsOneWidget);
     });
